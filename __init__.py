1411a89841178852bf57610cf216fa77796bbc03
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-31 16:50:40 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from mwdb.model import File
 from mwdblib import MWDB
 
 __author__ = "Askar Dyussekeyev"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __doc__ = "Local plugin for MWDB that scans (re-)uploaded using ClamAV and Yara"
 
 logger = logging.getLogger("mwdb.plugin.clamyara")
@@ -18,6 +18,8 @@ logger = logging.getLogger("mwdb.plugin.clamyara")
 config_api_url = ""
 config_api_key = ""
 SCAN_DIR = '/tmp/share'
+YARA_RULES_FILE = "yara-rules-full.yar"
+
 if not os.path.exists(SCAN_DIR):
     os.makedirs(SCAN_DIR)
 
@@ -52,6 +54,20 @@ def ClamVersion():
     )
     return result.stdout.strip()
 
+def YaraScan(file_path):
+    """Runs the yara program with specified parameters."""
+    result = subprocess.run(
+        ["yara", YARA_RULES_FILE, file_path], 
+        capture_output=True, text=True
+    )
+    return result.stdout.strip()
+
+def YaraParse(output):
+    """Parses the YARA output to find the rule matches."""
+    if not output:
+        return "Undetected"
+    return output.split("\n")
+
 def ClamYaraAddTag(file, av_name: str, av_result: str):
     for tag in file.tags:
         if tag.lower().startswith(f"{av_name.lower()}:"):
@@ -75,11 +91,17 @@ def ClamYaraProcessFile(hash_value):
     comment += f"ClamAV: {cl_result} (Version: {cl_version})\n"
     
     # Scan with Yara
+    yara_result = YaraScan(temp_file_path)
+    yara_matches = YaraParse(yara_result)
+    comment += f"YARA Matches: {', '.join(yara_matches)}\n" if yara_matches != "Undetected" else "YARA: Undetected\n"
     
     # Add results
     file.add_comment(comment.strip())
     if cl_result != 'Error' and cl_result != 'Undetected':
         ClamYaraAddTag(file, 'clamav', cl_result)
+    if yara_matches != "Undetected":
+        for match in yara_matches:
+            ClamYaraAddTag(file, 'yara', match)
     
     # Remove the temporary file
     os.remove(temp_file_path)
